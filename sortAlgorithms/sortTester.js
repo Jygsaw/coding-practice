70c2966b42a3e4642adcae97715c7a4c08ef664e
Justin Cheung <jcheung@crunchyroll.com>
2015-11-22 08:04:20 -0800

    [NEW] Implement sortTester.js

---
+++ sortAlgorithms/sortTester.js
@@ -0,0 +1,33 @@
+var sortTester = function (type, size) {
+  console.log("===== Testing:", type, "=====");
+  var sortFunc = require('./' + type + '/' + type + '.js');
+  var numUtils = require('../utils/genNumbers.js');
+
+  // generate random numbers
+  console.log(">>> generating numbers <<<");
+  var nums = numUtils.genNumArray(size || 10);
+  console.log("unsorted:", nums);
+
+  // sort numbers
+  console.log(">>> sorting numbers <<<");
+  var sorted = sortFunc(nums);
+  console.log("sorted:", sorted);
+
+  // verify results
+  console.log(">>> verifying sort <<<");
+  var verify = nums.slice().sort(function (a, b) {
+    return a - b;
+  });
+  console.log("unsorted:", nums);
+  console.log("verify:", verify);
+  if (sorted.length === verify.length &&
+      sorted.every(function (elem, index) {
+        return elem === verify[index];
+      })) {
+    console.log(">>> sort: SUCCESS <<<");
+  } else {
+    console.log(">>> sort: FAIL <<<");
+  }
+};
+
+module.exports = sortTester;
